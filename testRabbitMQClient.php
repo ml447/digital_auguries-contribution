7dc295fdc2e2e7304f1f5e68eef21e132da72810
ml447 <ml447@njit.edu>
2024-04-26 23:11:38 -0400

    Setup RabbitMQ Server Communication

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -5,19 +5,19 @@ require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-if (isset($argv[1]))
-{
-  $msg = $argv[1];
-}
-else
-{
+
+// Concatenate all command line arguments to form the message
+$msg = implode(" ", array_slice($argv, 1));
+
+// If no arguments are passed, set a default test message
+if (empty($msg)) {
   $msg = "test message";
 }
 
 $request = array();
 $request['type'] = "Login";
-$request['username'] = "steve";
-$request['password'] = "password";
+$request['username'] = "ml447";
+$request['password'] = "ml447";
 $request['message'] = $msg;
 $response = $client->send_request($request);
 //$response = $client->publish($request);
