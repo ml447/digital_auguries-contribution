efeafcfba45d2359bf87195c191dd6faedf35bc8
StephenSBC <145053737+StephenSBC@users.noreply.github.com>
2024-05-06 12:40:12 -0400

    Add files via upload

---
+++ reference.py
@@ -0,0 +1,22 @@
+file = open("formData.txt", "r")	  
+<input type="unameAuth">
+content = file.read();
+file.close;
+/*
+	  <input type="passAuth">
+		//Unameauth = line 1
+		//passAuth = line 2
+		//then an if statement here
+    </div>
+
+
+        // First find a way to refernce formData.txt (this is done on line 45)
+		//Then have the first line be sliced and set equal to Username
+		//Then have the second line be sliced and set equal to Password
+		//From there have an if statement
+		//If U1 = Usernmane && PS = Password 
+		//Submit button lets them into test website
+		//have a button to log them out.
+		//I need to make sure that they can only get in if that test website works however
+		
+        */
